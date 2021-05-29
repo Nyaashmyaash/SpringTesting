8d36f07f0d133cb150464a79c6c56cc162dcfdff
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-05-30 03:48:05 +0700

    add changes

--- src/main/java/com/nyash/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/com/nyash/aop/aspects/UniversityLoggingAspect.java
@@ -2,6 +2,7 @@ package com.nyash.aop.aspects;
 
 import com.nyash.aop.Student;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -17,26 +18,32 @@ public class UniversityLoggingAspect {
         System.out.println("beforeGetStudentsLoggingAdvice: before getStudents method -  Students list log");
     }
 
-    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
-    public void afterReturningGetStudentsLoggingAdvice(List<Student> students){
+    @AfterThrowing("execution(* getStudents())")
+    public void afterThrowingGetStudentsLoggingAdvice(){
+        System.out.println("afterThrowingGetStudentsLoggingAdvice: throwing exceptions log");
+    }
+
+//    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
+//    public void afterReturningGetStudentsLoggingAdvice(List<Student> students){
 
         //-------------------------------
         //замена результата таргет метода
-        Student firstStudent = students.get(0);
-        String nameSurname = firstStudent.getNameSurname();
-        nameSurname = "Mr." + nameSurname;
-        firstStudent.setNameSurname(nameSurname);
-
-        double avgGrade = firstStudent.getAvgGrade();
-        avgGrade = avgGrade + 1;
-        firstStudent.setAvgGrade(avgGrade);
-
-        System.out.println("afterReturningGetStudentsLoggingAdvice: after getStudents method -  Students list log");
-    }
+//        Student firstStudent = students.get(0);
+//        String nameSurname = firstStudent.getNameSurname();
+//        nameSurname = "Mr." + nameSurname;
+//        firstStudent.setNameSurname(nameSurname);
+//
+//        double avgGrade = firstStudent.getAvgGrade();
+//        avgGrade = avgGrade + 1;
+//        firstStudent.setAvgGrade(avgGrade);
+//
+//        System.out.println("afterReturningGetStudentsLoggingAdvice: after getStudents method -  Students list log");
+//    }
 
 
 //    @AfterReturning("execution(* getStudents())")
 //    public void afterReturningGetStudentsLoggingAdvice(){
 //        System.out.println("afterReturningGetStudentsLoggingAdvice: after getStudents method -  Students list log");
+
 //    }
 }
